8aa8bf78d9506d1bc11f6142e01d2dd2c5a63b0d
alexabotello <abotello20@yahoo.com>
2020-12-07 18:58:35 -0600

    update and delete

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -8,9 +8,10 @@ class GardensController < ApplicationController
     end 
 
     def create
-        byebug 
         merged_params = garden_params.merge(user_id: current_user.id)
         @garden = Garden.new(merged_params)
+        # @plant = Plant.find(merged_params[:plant_ids])
+        # @garden.plants << @plant
         if @garden.save
           redirect_to garden_path(@garden)
         else 
@@ -18,7 +19,27 @@ class GardensController < ApplicationController
         end 
     end 
 
+    def edit 
+        @garden = Garden.find(params[:id])
+    end 
+
+    def update
+        @garden = Garden.find(params[:id])
+        if @garden.update(garden_params)
+            redirect_to garden_path(@garden)
+        else 
+            render :edit
+        end
+    end 
+    
+    def destroy
+        @garden = Garden.find(params[:id])
+        @garden.destroy
+        redirect_to root_path
+    end 
+
+    private 
     def garden_params 
-        params.require(:garden).permit(:user_id, :plant_id)
+        params.require(:garden).permit(:user_id, :plant_ids, :garden_name)
     end
 end
